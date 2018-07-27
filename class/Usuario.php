55438e54b29b52e342c3b11f61f7ac66b6fe7702
Wilson Mesquita <wylllson@gmail.com>
2018-07-27 20:00:13 -0300

    Alterando a classe usuário

--- class/Usuario.php
+++ class/Usuario.php
@@ -1,7 +1,55 @@
 <?php
 
 class Usuario {
-    //...    
+
+    private $idusuario;
+    private $deslogin;    
+    private $dessenha;    
+    private $dtcadastro;
+
+    public function getIdusuario(){
+        return $this->idusuario;
+    }
+
+    public function setIdusuario($value){
+        $this->idusuario = $value;
+    }
+
+    public function getDeslogin(){
+        return $this->deslogin;
+    }
+
+    public function setDeslogin($value){
+        $this->deslogin = $value;
+    }
+
+    public function getDessenha(){
+        return $this->dessenha;
+    }
+
+    public function setDessenha($value){
+        $this->dessenha = $value;
+    }
+
+    public function getDatacadastro(){
+        return $this->datacadastro;
+    }
+
+    public function setDatacadastro($value){
+        $this->datacadastro = $value;
+    }
+
+    public function loadById($id){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+            ":ID"=>$id
+        ));
+
+        if(count($results) > 0){
+            $row = $results[0];
+            
+        }
+    }
 }
 
 ?>
