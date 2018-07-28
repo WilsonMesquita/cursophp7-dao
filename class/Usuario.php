665d0776dbad97013be09d4fba6d8adc7a0f520f
Wilson Mesquita <wylllson@gmail.com>
2018-07-27 21:05:37 -0300

    Novas atualiações

--- class/Usuario.php
+++ class/Usuario.php
@@ -31,12 +31,12 @@ class Usuario {
         $this->dessenha = $value;
     }
 
-    public function getDatacadastro(){
-        return $this->datacadastro;
+    public function getDtcadastro(){
+        return $this->dtcadastro;
     }
 
-    public function setDatacadastro($value){
-        $this->datacadastro = $value;
+    public function setDtcadastro($value){
+        $this->dtcadastro = $value;
     }
 
     public function loadById($id){
@@ -45,10 +45,24 @@ class Usuario {
             ":ID"=>$id
         ));
 
-        if(count($results) > 0){
+        if (count($results) > 0){
             $row = $results[0];
-            
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
         }
+    }
+
+    public function __toString(){
+
+        return json_encode(array(
+            "idsusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "dessenha"=>$this->getDessenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+        ));
     }
 }
 
--- index.php
+++ index.php
@@ -1,11 +1,10 @@
 <?php
-
-    require_once("config.php");
-
+    /*require_once("config.php");
     $sql = new Sql();
-    
     $usuarios = $sql->select("SELECT * FROM tb_usuarios");
-
-    echo json_encode($usuarios);
-
+    echo json_encode($usuarios);*/
+    require_once("config.php");
+    $root = new Usuario();
+    $root->loadById(2);
+    echo $root;
 ?>
